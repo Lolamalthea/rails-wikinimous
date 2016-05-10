9bac11eef8fa898dc80c21b2e76dc5e018e43a97
Laurence Crevel <laurence@a-nounou.com>
2016-05-10 16:22:32 +0200

    Fake Articles created.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Faker::Hipster.sentence
+Faker::Hipster.paragraph
